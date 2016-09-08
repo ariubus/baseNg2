3359ebcca94f63a3171b6c8c9c22b85318c4dc32
Ariel F. Cabañas <ariubus@gmail.com>
2016-09-07 22:46:43 -0400

    as

--- baseNg2/gulpfile.js
+++ baseNg2/gulpfile.js
@@ -6,7 +6,7 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 var gulp = require('gulp');
 var _ = require('lodash');
 
-var ng2 = ['node_modules/angular2/bundles/*.*',
+var ng2 = ['node_modules/angular2/bundles/*.*', 
            'node_modules/systemjs/dist/*.*',
            'node_modules/rxjs/bundles/*.*',
            'node_modules/es6-shim/es6-shim.js'];
